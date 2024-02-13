94dccb0a8a60ac07d080c273b980083fe14133eb
SarabojDev <saraboj.sidharth@gmail.com>
2024-02-13 12:54:11 +0530

    s1 first commit

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ console.log("HELLO WORLD SARABOJ RE-CHANGE THE FILE!")
 const number1 = 10;
 const number2 =15;
 const total =  number1  + number2;
-console.log(total)
+console.log(total,"Total amount in console")
